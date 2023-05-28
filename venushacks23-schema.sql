54d79c301abf55d251775de5e8febf9d87583d2a
Annesa Tran <annesat2020@gmail.com>
2023-05-27 22:10:26 -0700

    New database updates

--- venushacks23-schema.sql
+++ venushacks23-schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS products;
+DROP TABLE products
 CREATE TABLE products (
     id              SERIAL PRIMARY KEY,
     name            TEXT NOT NULL,
@@ -7,12 +7,12 @@ CREATE TABLE products (
     image           TEXT NOT NULL,
     ingredients     TEXT NOT NULL,
     safety          TEXT NOT NULL,
-    oily            BOOLEAN NOT NULL DEFAULT 0,
-    dry             BOOLEAN NOT NULL DEFAULT 0,
-    sensitive       BOOLEAN NOT NULL DEFAULT 0,
+    oily            INTEGER NOT NULL DEFAULT 0,
+    dry             INTEGER NOT NULL DEFAULT 0,
+    sensitive       INTEGER NOT NULL DEFAULT 0,
     comedogenic     INTEGER NOT NULL DEFAULT 0,
     acne_fighting   INTEGER NOT NULL DEFAULT 0,
     anti_aging      INTEGER NOT NULL DEFAULT 0,
     brightening     INTEGER NOT NULL DEFAULT 0,
     uv              INTEGER NOT NULL DEFAULT 0        
-);
+)
