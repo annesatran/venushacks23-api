392fac02456324731a37475174e68de003f8bed4
Annesa Tran <annesat2020@gmail.com>
2023-05-27 17:51:22 -0700

    Progress on product recommendation algorithm

--- models/products.js
+++ models/products.js
@@ -5,10 +5,43 @@ const { BadRequestError } = require("../utils/errors")
 class Products {
 
     static async fetchRecommended(needs) {
-        if (!(needs.routine && needs.skintype && needs.concerns)) {
-            throw new BadRequestError("Invalid request body")
-        }        
 
+        // check that all required fields are there / is not a malformed request body
+        const requiredFields = ["cleanser", "toner", "serum", "moisturizer", "sunscreen", 
+                                "safety", "comedogenic",
+                                "oily", "dry", "sensitive", 
+                                "acne-fighting", "anti-aging", "brightening", "uv"]
+        requiredFields.forEach((field) => {
+            if (!needs.hasOwnProperty(field)) {
+                throw new BadRequestError("Invalid request body")
+            }
+        })
+
+        var productRecs = { // initialize return JSON
+            "cleanser": null,
+            "toner": null, 
+            "serum": null,
+            "moisturizer": null,
+            "sunscreen": null
+        }
+
+        // algorithm to produce query string that matches skintype
+        var skintypes = ["oily", "dry", "sensitive"].filter((type) => eval("needs."+type))
+        
+
+        // algorithm to produce query string that matches skin needs
+
+        // loop through each skincare category and produce results
+        for (const [key, _] of Object.entries(productRecs)) {
+            if (eval("needs."+key)) {
+                const query = `
+                    SELECT * FROM products
+                    WHERE type = $1 AND
+                `
+                var result = await db.query(query, [key])
+                eval("productRecs."+key+" = "+"result.rows") // update product recs JSON
+            }
+        }
         return productRecs
     }
 
