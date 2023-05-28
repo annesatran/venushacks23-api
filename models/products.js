54e7e49a31835a02494eabc0a367a7504277bec4
Annesa Tran <annesat2020@gmail.com>
2023-05-27 22:36:18 -0700

    Finished updates to ranking algorithm

--- models/products.js
+++ models/products.js
@@ -8,9 +8,8 @@ class Products {
 
         // check that all required fields are there / is not a malformed request body
         const requiredFields = ["cleanser", "toner", "serum", "moisturizer", "sunscreen", 
-                                "safety", "comedogenic",
-                                "oily", "dry", "sensitive", 
-                                "acne_fighting", "anti_aging", "brightening", "uv"]
+        "oily", "dry", "sensitive", 
+        "acne_fighting", "anti_aging", "brightening", "uv"]
         requiredFields.forEach((field) => {
             if (!needs.hasOwnProperty(field)) {
                 throw new BadRequestError("Invalid request body")
@@ -27,25 +26,25 @@ class Products {
         }
 
         // algorithm to produce query string that filters skintype
-        var chosenSkintypes = ["oily", "dry", "sensitive"].filter((type) => eval("needs."+type))
-        const skintypeQueryMap = chosenSkintypes.map(skintype => skintype+" is TRUE")
+        var chosenSkintypes = ["oily", "dry", "sensitive"].filter((type) => eval("needs."+type+" == 1"))
+        const skintypeQueryMap = chosenSkintypes.map(skintype => `"${skintype}" = 1`)
         const skintypeQuery = skintypeQueryMap.length > 0 ? "AND ("+ skintypeQueryMap.join(" OR ")+")" : ""
 
         // algorithm to produce query string that ranks product by skin needs
         var chosenSkinNeeds = ["acne_fighting", "anti_aging", "brightening", "uv"].filter((need) => eval("needs."+need))
+        chosenSkinNeeds = chosenSkinNeeds.map((need) => `"${need}"`)
         var skinNeedsQuery = chosenSkinNeeds > 0 ? chosenSkinNeeds.join(" + ") : "0" 
-        ///////// To-do : check
-        skinNeedsQuery += (needs.oily == true) ? " + comedogenic" : ""
+        skinNeedsQuery += (needs.oily == true) ? ` + "comedogenic"` : ""
 
         // loop through each skincare category and produce results
         for (const [key, _] of Object.entries(productRecs)) {
             if (eval("needs."+key)) {
                 const query = 
-                    `SELECT *, SUM(${skinNeedsQuery}) AS ranking   
+                    `SELECT *, SUM(${skinNeedsQuery}) AS "ranking"  
                     FROM products
-                    WHERE type = $1 AND safety >= "50"
-                    ${skintypeQuery}
-                    ORDER BY ranking
+                    WHERE "type" = $1 AND "safety" >= '50' ${skintypeQuery}
+                    GROUP BY "id"
+                    ORDER BY "ranking"
                     LIMIT 20`
                 var result = await db.query(query, [key])
                 eval("productRecs."+key+" = "+"result.rows") // update product recs JSON
