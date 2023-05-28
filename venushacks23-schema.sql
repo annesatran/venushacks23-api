333d5dd9ef744493f7525a9ba66e593108752c9a
Annesa Tran <annesat2020@gmail.com>
2023-05-27 17:50:38 -0700

    Update database schema

--- venushacks23-schema.sql
+++ venushacks23-schema.sql
@@ -1,22 +1,19 @@
+DROP TABLE IF EXISTS products;
 
 CREATE TABLE products (
-    id           SERIAL PRIMARY KEY,
-    name         TEXT NOT NULL,
-    brand        TEXT NOT NULL,
-    type         TEXT NOT NULL,
-    image        TEXT NOT NULL,
-    ingredients  TEXT NOT NULL
-);
-
-CREATE TABLE ingredients (
     id              SERIAL PRIMARY KEY,
-    name            TEXT NOT NULL UNIQUE,
-    safety          INTEGER NOT NULL CHECK (safety BETWEEN 1 and 10),
-    comedogenic     INTEGER NOT NULL CHECK (comedogenic BETWEEN 1 and 5),
-    acne_fighting   BOOLEAN NOT NULL DEFAULT FALSE,
-    anti_aging      BOOLEAN NOT NULL DEFAULT FALSE,
-    brightening     BOOLEAN NOT NULL DEFAULT FALSE,
+    name            TEXT NOT NULL,
+    brand           TEXT NOT NULL,
+    type            TEXT NOT NULL,
+    image           TEXT NOT NULL,
+    ingredients     TEXT NOT NULL,
+    safety          TEXT NOT NULL,
+    comedogenic     BOOLEAN NOT NULL DEFAULT FALSE,
     oily            BOOLEAN NOT NULL DEFAULT FALSE,
     dry             BOOLEAN NOT NULL DEFAULT FALSE,
-    sensitive       BOOLEAN NOT NULL DEFAULT FALSE
-)
+    sensitive       BOOLEAN NOT NULL DEFAULT FALSE,
+    acne-fighting   BOOLEAN NOT NULL DEFAULT FALSE,
+    anti_aging      BOOLEAN NOT NULL DEFAULT FALSE,
+    brightening     BOOLEAN NOT NULL DEFAULT FALSE,
+    uv              BOOLEAN NOT NULL DEFAULT FALSE          
+);
